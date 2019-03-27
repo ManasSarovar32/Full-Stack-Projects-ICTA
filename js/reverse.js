89f605a7f3b5b52db56eb7d3cdf3611dd6048175
ManasSarovar32 <manassarovar32@gmail.com>
2019-03-27 10:51:43 +0530

    program using function in js updated

--- js/reverse.js
+++ js/reverse.js
@@ -2,15 +2,19 @@ var num=132
 var rev=0;
 var rem=0;
 
-var res=reverse(num,rev,rem);
+
+var res=reverse(num);
 console.log(rev);
 
-function reverse(x,y,z)
+function reverse(x)
 {
+    
+
     while (num>0)
     {
     rem=num%10;
     rev=rev*10+rem;
     num=parseInt(num/10);
     }
+    return rev;
 }
